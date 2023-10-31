cb0e8378775656b53622965f436051b54a11130e
Mary-WJ <Mary-Par@live.nmit.ac.nz>
2023-11-01 06:04:13 +1300

    firebase sdk file

---
+++ src/firebase-sdk.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGYKxUd3tZlY2YjjQOM3DUU3ifcLQjTGE",
+  authDomain: "webportfolio-project.firebaseapp.com",
+  projectId: "webportfolio-project",
+  storageBucket: "webportfolio-project.appspot.com",
+  messagingSenderId: "401120673449",
+  appId: "1:401120673449:web:3538ca9616734fb6f0b3b8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
