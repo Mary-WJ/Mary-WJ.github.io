332cd61e1455478f57784c4295f694851449e941
Mary-WJ <Mary-Par@live.nmit.ac.nz>
2023-11-03 07:46:29 +1300

    Webpack is added and comment section is complete, as well as some minor change here and there

--- src/app.js
+++ src/app.js
@@ -1,1 +1,87 @@
-console.log('hello of app.s')
+import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import {db, app} from './firebase-sdk.js'
+
+
+
+const colRef = collection(db, 'Comments');
+
+    getDocs(colRef).then((snapshot) => {
+        let comments = [];
+        //docs represent all of the document
+        snapshot.docs.forEach((doc) => {
+        comments.push({...doc.data(), id: doc.id})
+        })
+        // console.log(comments)
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
+const displayCommentUI = document.getElementById('display-comment');
+const addComment = document.getElementById('formSubmit');
+
+if (addComment) {
+    addComment.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let textComment = addComment.comment.value;
+
+        const li = document.createElement('li');
+        const btn = document.createElement('button');
+        const i = document.createElement('i');
+
+        addDoc(colRef, {
+            comment: textComment,
+        })
+        .then((docRef) => {
+            const commentId = docRef.id;
+            li.setAttribute('data-id', commentId);
+
+            btn.classList.add('deleteList');
+            i.className = "fa-solid fa-trash-can";
+            btn.appendChild(i);
+
+            li.classList.add('userComment');
+            li.textContent = textComment;
+            li.appendChild(btn);
+
+            displayCommentUI.appendChild(li);
+
+            //clear input of textarea
+            addComment.reset();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    });
+}
+
+// Delete list item using event delegation
+displayCommentUI.addEventListener('click', (e) => {
+    if (e.target.classList.contains('deleteList') || e.target.parentElement.classList.contains('deleteList')) {
+        e.preventDefault();
+
+        const liToDelete = e.target.closest('.userComment');
+        const commentId = liToDelete.getAttribute('data-id');
+        
+        const docRef = doc(db, 'Comments', commentId);
+
+        deleteDoc(docRef).then(() => {
+            liToDelete.remove();
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+});
+
+
+
+ 
+
+
+
+  
+  
+
+
--- src/firebase-sdk.js
+++ src/firebase-sdk.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc} from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,4 +18,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+const db = getFirestore(app);
+
+
+export{
+  db,
+  app
+}
 
