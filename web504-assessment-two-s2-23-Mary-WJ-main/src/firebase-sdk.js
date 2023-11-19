f7783704c882dca9b66c643b26f6a8225ef5e059
Mary-WJ <125645377+Mary-WJ@users.noreply.github.com>
2023-11-19 22:50:25 +1300

    Add files via upload

---
+++ web504-assessment-two-s2-23-Mary-WJ-main/src/firebase-sdk.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGYKxUd3tZlY2YjjQOM3DUU3ifcLQjTGE",
+  authDomain: "webportfolio-project.firebaseapp.com",
+  projectId: "webportfolio-project",
+  storageBucket: "webportfolio-project.appspot.com",
+  messagingSenderId: "401120673449",
+  appId: "1:401120673449:web:3538ca9616734fb6f0b3b8"
+};
+
+// Initialize and get reference to the service
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth();
+
+
+export{
+  db,
+  app,
+  auth
+}
+
