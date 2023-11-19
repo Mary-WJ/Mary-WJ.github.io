4630f15a59ce4ad210f01624680a8a72cf5eeaae
Mary-WJ <125645377+Mary-WJ@users.noreply.github.com>
2023-11-20 00:18:30 +1300

    Delete src directory

--- src/side.js
+++
@@ -1,9 +0,0 @@
-let activePage = window.location.pathname;
-//check the current location pathname
-const navLinks = document.querySelectorAll('nav a');
-navLinks.forEach(link => {
-    //if the link href contains the same value of activePage then add class called 'active'
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('active');
-    }
-})
